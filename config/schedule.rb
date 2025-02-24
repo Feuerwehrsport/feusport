b7e0a6dec07154ca4fdde003986319cc67e11da2
Georg Limbach <georg.limbach@lichtbit.com>
2025-02-24 16:22:40 +0100

    Crontabs in SolidQueue überführen

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,11 +11,4 @@ if environment == 'production'
     #     rails_command 'debug:failed_delayed_jobs'
     rails_command 'rails_log_parser:parse[22]'
   end
-
-  every :day, at: '5:21 am' do
-    rails_command 'runner ImportSuggestionsJob.perform_later'
-  end
-  every :tuesday, at: '10:00 am' do
-    rails_command 'runner Competitions::Publishing::ReminderJob.perform_later'
-  end
 end
