09e51a3c0504b266666aafba8d1f0ba107430bc8
Georg Limbach <georg.limbach@lichtbit.com>
2025-05-18 20:50:31 +0200

    Hinweis-Log ignorieren

--- config/rails_log_parser.rb
+++ config/rails_log_parser.rb
@@ -4,5 +4,6 @@ RailsLogParser.configure do |parser|
   parser.ignore_lines = [
     /SolidQueue.* Fail claimed jobs .* job_ids: \[\], process_ids: \[\]/,
     /WebSocket error occurred: Broken pipe/,
+    /Ignoring message processed after the WebSocket was closed/,
   ]
 end
