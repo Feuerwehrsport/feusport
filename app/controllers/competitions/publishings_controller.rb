a692da4903bbfd564c333215ea1e2d5c34b1da74
Georg Limbach <georg.limbach@lichtbit.com>
2025-06-03 13:23:28 +0200

    Publishing-Controller besser abfangen

--- app/controllers/competitions/publishings_controller.rb
+++ app/controllers/competitions/publishings_controller.rb
@@ -4,8 +4,6 @@ class Competitions::PublishingsController < CompetitionNestedController
   before_action :assign_new_resource
 
   def create
-    authorize!(:edit, @competition)
-
     @publishing.assign_attributes(publishing_params)
     if @publishing.save
       redirect_to competition_show_path, notice: :deleted
@@ -18,6 +16,7 @@ class Competitions::PublishingsController < CompetitionNestedController
   protected
 
   def assign_new_resource
+    authorize!(:edit, @competition)
     @publishing = Competitions::Publishing.new(competition: @competition, user: current_user)
   end
 
