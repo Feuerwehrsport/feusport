8aaedcbaa95f0e0da7e6d6f509086218721b64d4
Georg Limbach <georg.limbach@lichtbit.com>
2025-07-16 12:59:00 +0200

    Rubocop

--- spec/models/score/competition_result_same_la_spec.rb
+++ spec/models/score/competition_result_same_la_spec.rb
@@ -23,92 +23,94 @@
 #
 require 'rails_helper'
 
-RSpec.describe 'Score::CompetitionResult with same LA' do
-  let!(:competition) { create(:competition) }
-  let!(:band) { create(:band, :female, competition:) }
+RSpec.describe Score::CompetitionResult do
+  context 'with same LA' do
+    let!(:competition) { create(:competition) }
+    let!(:band) { create(:band, :female, competition:) }
 
-  let!(:la) { create(:discipline, :la, competition:) }
-  let!(:gs) { create(:discipline, :gs, competition:) }
+    let!(:la) { create(:discipline, :la, competition:) }
+    let!(:gs) { create(:discipline, :gs, competition:) }
 
-  let!(:assessment_la) { create(:assessment, competition:, discipline: la, band:) }
-  let!(:assessment_gs) { create(:assessment, competition:, discipline: gs, band:) }
+    let!(:assessment_la) { create(:assessment, competition:, discipline: la, band:) }
+    let!(:assessment_gs) { create(:assessment, competition:, discipline: gs, band:) }
 
-  let!(:result_la) { create(:score_result, competition:, assessment: assessment_la) }
-  let!(:result_gs) { create(:score_result, competition:, assessment: assessment_gs) }
+    let!(:result_la) { create(:score_result, competition:, assessment: assessment_la) }
+    let!(:result_gs) { create(:score_result, competition:, assessment: assessment_gs) }
 
-  let!(:team1) { create(:team, competition:, band:) }
-  let!(:team2) { create(:team, competition:, band:) }
-  let!(:team3) { create(:team, competition:, band:) }
-  let!(:team4) { create(:team, competition:, band:) }
+    let!(:team1) { create(:team, competition:, band:) }
+    let!(:team2) { create(:team, competition:, band:) }
+    let!(:team3) { create(:team, competition:, band:) }
+    let!(:team4) { create(:team, competition:, band:) }
 
-  let!(:list_la) do
-    create_score_list(result_la,
-                      team1 => 2200, team2 => 2200, team3 => nil, team4 => nil)
-  end
-  let!(:list_gs) do
-    create_score_list(result_gs,
-                      team2 => 2200, team1 => 2200, team3 => nil, team4 => nil)
-  end
-  let!(:competition_result) do
-    create(:score_competition_result, competition:, results: [result_la, result_gs], result_type:)
-  end
-  let(:result_type) { 'places_to_points' }
-
-  it 'calculates correct results' do
-    rows = competition_result.rows
-    expect(rows[0].points).to eq 3
-    expect(rows[0].place).to eq 1
-    expect(rows[0].team).to be_in([team1, team2])
-    expect(rows[0].assessment_result_from(result_la).points).to eq 1
-    expect(rows[0].assessment_result_from(result_gs).points).to eq 2
-
-    expect(rows[1].points).to eq 3
-    expect(rows[1].place).to eq 1
-    expect(rows[1].team).to be_in([team1, team2])
-    expect(rows[1].assessment_result_from(result_la).points).to eq 2
-    expect(rows[1].assessment_result_from(result_gs).points).to eq 1
-
-    expect(rows[2].points).to eq 8
-    expect(rows[2].place).to eq 3
-    expect(rows[2].team).to be_in([team3, team4])
-    expect(rows[2].assessment_result_from(result_la).points).to eq 4
-    expect(rows[2].assessment_result_from(result_gs).points).to eq 4
-
-    expect(rows[3].points).to eq 8
-    expect(rows[3].place).to eq 3
-    expect(rows[3].team).to be_in([team3, team4])
-    expect(rows[3].assessment_result_from(result_la).points).to eq 4
-    expect(rows[3].assessment_result_from(result_gs).points).to eq 4
-  end
-
-  context 'when dcup' do
-    let(:result_type) { 'dcup' }
+    let!(:list_la) do
+      create_score_list(result_la,
+                        team1 => 2200, team2 => 2200, team3 => nil, team4 => nil)
+    end
+    let!(:list_gs) do
+      create_score_list(result_gs,
+                        team2 => 2200, team1 => 2200, team3 => nil, team4 => nil)
+    end
+    let!(:competition_result) do
+      create(:score_competition_result, competition:, results: [result_la, result_gs], result_type:)
+    end
+    let(:result_type) { 'places_to_points' }
 
     it 'calculates correct results' do
       rows = competition_result.rows
-      expect(rows[0].points).to eq 19
+      expect(rows[0].points).to eq 3
       expect(rows[0].place).to eq 1
       expect(rows[0].team).to be_in([team1, team2])
-      expect(rows[0].assessment_result_from(result_la).points).to eq 10
-      expect(rows[0].assessment_result_from(result_gs).points).to eq 9
+      expect(rows[0].assessment_result_from(result_la).points).to eq 1
+      expect(rows[0].assessment_result_from(result_gs).points).to eq 2
 
-      expect(rows[1].points).to eq 19
+      expect(rows[1].points).to eq 3
       expect(rows[1].place).to eq 1
       expect(rows[1].team).to be_in([team1, team2])
-      expect(rows[1].assessment_result_from(result_la).points).to eq 9
-      expect(rows[1].assessment_result_from(result_gs).points).to eq 10
+      expect(rows[1].assessment_result_from(result_la).points).to eq 2
+      expect(rows[1].assessment_result_from(result_gs).points).to eq 1
 
-      expect(rows[2].points).to eq 14
+      expect(rows[2].points).to eq 8
       expect(rows[2].place).to eq 3
       expect(rows[2].team).to be_in([team3, team4])
-      expect(rows[2].assessment_result_from(result_la).points).to eq 7
-      expect(rows[2].assessment_result_from(result_gs).points).to eq 7
+      expect(rows[2].assessment_result_from(result_la).points).to eq 4
+      expect(rows[2].assessment_result_from(result_gs).points).to eq 4
 
-      expect(rows[3].points).to eq 14
+      expect(rows[3].points).to eq 8
       expect(rows[3].place).to eq 3
       expect(rows[3].team).to be_in([team3, team4])
-      expect(rows[3].assessment_result_from(result_la).points).to eq 7
-      expect(rows[3].assessment_result_from(result_gs).points).to eq 7
+      expect(rows[3].assessment_result_from(result_la).points).to eq 4
+      expect(rows[3].assessment_result_from(result_gs).points).to eq 4
+    end
+
+    context 'when dcup' do
+      let(:result_type) { 'dcup' }
+
+      it 'calculates correct results' do
+        rows = competition_result.rows
+        expect(rows[0].points).to eq 19
+        expect(rows[0].place).to eq 1
+        expect(rows[0].team).to be_in([team1, team2])
+        expect(rows[0].assessment_result_from(result_la).points).to eq 10
+        expect(rows[0].assessment_result_from(result_gs).points).to eq 9
+
+        expect(rows[1].points).to eq 19
+        expect(rows[1].place).to eq 1
+        expect(rows[1].team).to be_in([team1, team2])
+        expect(rows[1].assessment_result_from(result_la).points).to eq 9
+        expect(rows[1].assessment_result_from(result_gs).points).to eq 10
+
+        expect(rows[2].points).to eq 14
+        expect(rows[2].place).to eq 3
+        expect(rows[2].team).to be_in([team3, team4])
+        expect(rows[2].assessment_result_from(result_la).points).to eq 7
+        expect(rows[2].assessment_result_from(result_gs).points).to eq 7
+
+        expect(rows[3].points).to eq 14
+        expect(rows[3].place).to eq 3
+        expect(rows[3].team).to be_in([team3, team4])
+        expect(rows[3].assessment_result_from(result_la).points).to eq 7
+        expect(rows[3].assessment_result_from(result_gs).points).to eq 7
+      end
     end
   end
 end
