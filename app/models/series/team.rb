537ec28379e2be1e4963e9c8c7bce3d90e68c98f
Georg Limbach <georg.limbach@lichtbit.com>
2026-03-19 21:45:39 +0100

    Code zusammenfassen

--- app/models/series/team.rb
+++ app/models/series/team.rb
@@ -1,18 +1,11 @@
 # frozen_string_literal: true
 
 class Series::Team
-  include ActiveModel::Model
-  include ActiveModel::Attributes
-  include Certificates::StorageSupport
+  include Series::EntitySupport
 
-  attr_accessor :config, :team, :team_number, :team_gender, :rank, :round
+  attr_accessor :team, :team_number, :team_gender
 
   delegate :id, to: :team, prefix: true
-
-  def initialize(*)
-    @rank = 0
-    super
-  end
 
   def full_name
     if multi_team?
@@ -26,24 +19,9 @@ class Series::Team
     round.team_participations.where(team_id:).where.not(team_number:).exists?
   end
 
-  def add_participation(participation)
-    @cups ||= {}
-    @cups[participation.cup_id] ||= []
-    @cups[participation.cup_id].push(participation)
-  end
-
   def participations_for_cup(cup)
     @cups ||= {}
     (@cups[cup.id] || []).sort_by(&:team_assessment)
-  end
-
-  def count
-    @cups ||= {}
-    @cups.values.count
-  end
-
-  def all_points
-    @cups.values.sum { |cup| cup.sum(&:points_with_correction) }
   end
 
   def points
@@ -66,58 +44,8 @@ class Series::Team
     end
   end
 
-  def second_best_time
-    best_result_entry
-  end
-
-  def second_sum_time
-    sum_result_entry
-  end
-
-  def best_result_entry
-    @best_result_entry ||= Score::ResultEntry.new(time_with_valid_calculation: best_time).human_time
-  end
-
-  def sum_result_entry
-    @sum_result_entry ||= Score::ResultEntry.new(time_with_valid_calculation: sum_time).human_time
-  end
-
-  def best_time_without_nil
-    best_time || (Firesport::INVALID_TIME + 1)
-  end
-
-  def sum_time
-    @sum_time ||= begin
-      sum = ordered_participations.sum(&:time)
-      [sum, Firesport::INVALID_TIME].min
-    end
-  end
-
-  def best_rank
-    @cups.values.flatten.map(&:rank).min
-  end
-
-  def best_rank_count
-    @cups.values.flatten.map(&:rank).count { |r| r == best_rank }
-  end
-
-  def ordered_participations
-    @ordered_participations ||= @cups.values.map(&:first).sort do |a, b|
-      compare = b.points_with_correction <=> a.points_with_correction
-      compare.zero? ? a.time <=> b.time : compare
-    end.first(config.calc_participations_count)
-  end
-
   def valid_participations
     @valid_participations ||= ordered_participations.select { |part| part.time < Firesport::INVALID_TIME }
-  end
-
-  def participation_count
-    @cups.values.count
-  end
-
-  def <=>(other)
-    config.sort(self, other)
   end
 
   def storage_support_get(position)
@@ -126,15 +54,7 @@ class Series::Team
       full_name
     when :person_name, :person_bib_number, :assessment_with_gender, :gender, :date, :place, :competition_name
       ''
-    when :rank
-      "#{rank}."
-    when :rank_with_rank
-      "#{rank}. Platz"
-    when :rank_with_rank2
-      "den #{rank}. Platz"
-    when :rank_without_dot
-      rank.to_s
-    when :assessment, :result_name
+    when :assessment
       round.name
     else
       super
