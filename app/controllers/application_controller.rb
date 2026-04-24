c3fb262ba3c1e67e871e5ad14305769098c93222
Georg Limbach <georg.limbach@lichtbit.com>
2026-04-24 13:33:36 +0200

    Domain immer überprüfen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,42 @@ class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
   include TabSessionIdSupport
 
+  before_action :ensure_correct_host!
+
   protected
 
   def default_url_options
     Rails.application.config.default_url_options
+  end
+
+  private
+
+  def ensure_correct_host!
+    expected =    Rails.application.config.default_url_options
+
+    return if expected.blank?
+
+    correct_host     = expected[:host].to_s
+    correct_port     = expected[:port].to_s
+    correct_protocol = expected[:protocol].to_s
+
+    current_host     = request.host.to_s
+    current_port     = request.port.to_s
+    current_protocol = request.protocol.delete("://").to_s
+
+    needs_redirect =
+      (correct_host.present? && current_host != correct_host) ||
+      (correct_port.present? && current_port != correct_port) ||
+      (correct_protocol.present? && current_protocol != correct_protocol)
+
+    if needs_redirect
+      redirect_to url_for(
+        params.permit!.to_h.merge(
+          host: correct_host,
+          port: correct_port,
+          protocol: correct_protocol
+        )
+      ), status: :moved_permanently
+    end
   end
 end
