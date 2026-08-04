a6c71603f8365cff425119d0c9ff73602d00b67c
Georg Limbach <georg.limbach@lichtbit.com>
2026-08-04 22:23:49 +0200

    fixup! Serienergebnisse für Urkunden korrigieren

--- app/models/series/team.rb
+++ app/models/series/team.rb
@@ -54,12 +54,6 @@ class Series::Team
       round.name
     when :result_name
       config.name
-    when :assessment_with_gender
-      config.name
-    when :gender
-      ""
-    when :date
-      I18n.l(competition.date)
     else
       super
     end
